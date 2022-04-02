59678e70039bc26e6d695d3a419ce9e1f3883e72
maddiethecafebabe <maddie@cafebabe.date>
2022-04-02 19:06:22 +0200

    discord

--- home/home.nix
+++ home/home.nix
@@ -1,14 +1,14 @@
 { config, user, pkgs, ... }:
 
 {
-  home.username = user.name;
-  home.homeDirectory = user.home;
+    imports = [
+      ./discord.nix
+    ];
 
-  home.packages = with pkgs; [
-    discord discord-canary
-  ];
+    home.username = user.name;
+    home.homeDirectory = user.home;
 
-  home.stateVersion = "22.05";
+    home.stateVersion = "22.05";
 
-  programs.home-manager.enable = true;
+    programs.home-manager.enable = true;
 }
