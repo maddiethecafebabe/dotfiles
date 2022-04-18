7b0a560f8fd46508c5c4f49e5f5fb79638c8ce0f
maddiethecafebabe <maddie@cafebabe.date>
2022-04-18 04:18:20 +0200

    boop

--- overlays/discord-canary.nix
+++ overlays/discord-canary.nix
@@ -1,3 +1,7 @@
+# discord-canary gets updated often and some updates ignore the SKIP_HOST_UPDATE setting..
+# TODO: the sha256 argument is optional and may be more convenient being left out? not sure 
+# how often nix will try to refetch it tbh, that may be something to look into
+
 self: super: { 
     discord-canary = super.discord-canary.overrideAttrs (
         old: { 
