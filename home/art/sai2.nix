62938219e1e08bfe2d6b28815b9852ff387944f9
maddiethecafebabe <maddie@cafebabe.date>
2022-04-27 07:58:42 +0200

    cleanup sai2.nix a bit

--- home/art/sai2.nix
+++ home/art/sai2.nix
@@ -1,27 +1,29 @@
+# i use sai2 too but i cant really bundle it as a package (commercial + needs device unique license file next to it)
+# so this will just expect ~/.local/Tools/Art/sai2/sai2.exe to be present
+
 { home, pkgs, user, ... }:
 
-{
+let
+    wine = pkgs.wineWowPackages.stable;
+
+    sai2 = pkgs.makeDesktopItem {
+        name = "sai2";
+        desktopName = "Paint Tool Sai 2";
+        exec = "${wine}/bin/wine ${user.home}/.local/Tools/Art/sai2/sai2.exe";
+        categories = [ "Art" "Graphics" ];
+        icon = "sai2";
+    };
+in {
     home.packages = with pkgs; [
         libresprite
         krita
-        wineWowPackages.stable
+        wine
         winetricks
+        sai2
     ];
 
-    # i use sai2 too but i cant really bundle it as a package (commercial + needs device unique license file next to it)
-    # so this will just expect ~/.local/Tools/Art/sai2/sai2.exe to be present
+    # TODO: thats a hack but idk how to best bundle icons
     home.file = {
-        ".local/share/applications/sai2.desktop".text = ''
-[Desktop Entry]
-Name=sai2
-Exec=/usr/bin/env wine ${user.home}/.local/Tools/Art/sai2/sai2.exe
-Type=Application
-StartupNotify=true
-Icon=sai2
-StartupWMClass=sai2.exe
-        '';
         ".local/share/icons/sai2.png".source = ./sai2.png;
     };
-
-
 }
