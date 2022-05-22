384489892cb3f38c07a8fff2f6603399d4de024f
maddiethecafebabe <maddie@cafebabe.date>
2022-05-22 19:58:36 +0200

    a

--- hosts/seifuku/configuration.nix
+++ hosts/seifuku/configuration.nix
@@ -1,44 +1,43 @@
+{ config, pkgs, lib, modules, ... }:
 
-  { config, pkgs, lib, modules, ... }:
-
-  let
+let
     hostName = "seifuku";
-  in {
+in {
     imports = [
-	    ./hardware-configuration.nix
-      ./security.nix
-      ./fs.nix
-      ./samba.nix
+        ./hardware-configuration.nix
+        ./security.nix
+        ./fs.nix
+        ./samba.nix
     ];
 
     modules = {
-      core = {
-        nix-ld.enable = false;
-        boot.systemd-boot.enable = false;
-      };
-      desktop.enable = false;
-      server = {
-        jellyfin = {
-          enable = true;
-          domain = "media.seifuku.local";
+        core = {
+            nix-ld.enable = false;
+            boot.systemd-boot.enable = false;
         };
-        sonarr = {
-          enable = true;
-          domain = "sonarr.seifuku.local";
+        desktop.enable = false;
+        server = {
+            jellyfin = {
+                enable = true;
+                domain = "media.seifuku.local";
+            };
+            sonarr = {
+                enable = true;
+                domain = "sonarr.seifuku.local";
+            };
         };
-      };
     };
 
     networking = {
-      inherit hostName;
-      wireless = {
-        enable = true;
-        userControlled.enable = true;
-      };
+        inherit hostName;
+        wireless = {
+            enable = true;
+            userControlled.enable = true;
+        };
     };
 
     # Enable GPU acceleration
     hardware.raspberry-pi."4".fkms-3d.enable = true;
 
     hardware.pulseaudio.enable = true;
-  }
+}
