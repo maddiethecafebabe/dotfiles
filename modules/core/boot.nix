eef890de3fd7e35e2cc0d9134c703237d5b2a3dd
maddiethecafebabe <maddie@cafebabe.date>
2022-05-22 16:04:42 +0200

    this is getting annoying

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -5,11 +5,11 @@ let
 in {
     options.modules.core.boot = {
         enable = mkOption { type = types.bool; default = true; };
-        systemd.enable = mkOption { type = types.bool; default = true; };
+        systemd-boot.enable = mkOption { type = types.bool; default = true; };
     };
 
     config = mkIf cfg.enable {
-        boot.loader.systemd-boot.enable = cfg.systemd.enable;
+        boot.loader.systemd-boot.enable = cfg.systemd-boot.enable;
 
         boot.loader.efi.canTouchEfiVariables = true;
         boot.loader.grub.useOSProber = true;
