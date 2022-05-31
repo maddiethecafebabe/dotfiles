a9247283d7d2953e6bafa021fad14d1260df0b39
maddiethecafebabe <maddie@cafebabe.date>
2022-05-31 05:41:01 +0200

    explain what im doing

--- home/dev/rust.nix
+++ home/dev/rust.nix
@@ -8,6 +8,11 @@
             "${user.home}/.cargo/bin"
         ];
 
+        # since rust is a language where projects can easily go into the 
+        # triple digits of dependencies linking takes quite a while and makes out
+        # most of rebuilds. 
+        # this sets it to use the mold linker (https://github.com/rui314/mold)
+        # which is a lot faster than the alternatives
         file.".cargo/config.toml".text = with pkgs; ''
         [target.x86_64-unknown-linux-gnu]
         linker = "${clang}/bin/clang"
