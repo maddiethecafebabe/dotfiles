eb2acf155b539c1fccc9a377b786ee4ce0b53c98
maddiethecafebabe <maddie@cafebabe.date>
2022-07-02 10:58:18 +0200

    split off dns config

--- modules/core/default.nix
+++ modules/core/default.nix
@@ -21,6 +21,7 @@ in {
 
   imports = [
     ./nix.nix
+    ./dns.nix
     ./xdg.nix
     ./ssh.nix
     ./boot.nix
@@ -47,8 +48,6 @@ in {
       direnv
     ];
 
-    environment.variables.EDITOR = "vim";
-
     programs.nix-ld.enable = cfg.nix-ld.enable;
 
     programs.bash.shellInit = ''
@@ -59,27 +58,6 @@ in {
       opt_source "$HOME/.profile"
       opt_source "/etc/profiles/per-user/$USER/etc/profile.d/hm-session-vars.sh"
     '';
-
-    networking.extraHosts = ''
-      192.168.0.100   media.seifuku.local radarr.seifuku.local sonarr.seifuku.local documents.seifuku.local
-      127.0.0.1   sonarr.kimono.local
-      ::1         sonarr.kimono.local
-    '';
-
-    networking.nameservers = [
-        "192.168.0.100"
-        
-        # quad9
-        "9.9.9.10"
-        "149.112.112.10"
-        "2620:fe::10"
-        "2620:fe::fe:10"
-    ];
-
-    networking.resolvconf.enable = pkgs.lib.mkForce false;
-    networking.dhcpcd.extraConfig = "nohook resolv.conf";
-    networking.networkmanager.dns = "none";
-    services.resolved.enable = false;
 
     # This value determines the NixOS release from which the default
     # settings for stateful data, like file locations and database versions
---
+++ modules/core/dns.nix
@@ -0,0 +1,20 @@
+{
+    networking.extraHosts = ''
+      192.168.0.100   media.seifuku.local radarr.seifuku.local sonarr.seifuku.local documents.seifuku.local
+      127.0.0.1   sonarr.kimono.local
+      ::1         sonarr.kimono.local
+    '';
+
+    networking.nameservers = [ "192.168.0.100" ];
+
+    services.resolved = {
+        enable = true;
+        fallbackDns = [   
+            # quad9
+            "9.9.9.10"
+            "149.112.112.10"
+            "2620:fe::10"
+            "2620:fe::fe:10"
+        ];
+    };
+}
